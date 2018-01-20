fb4202501c8b2b899f0b3683d4b2a915b6449cc9
M.Demchenko <m.demchenko@program-ace.com.ua>
2018-01-20 19:29:05 +0200

    Add js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,80 @@ var helloWorld = function() {
 };
 
 helloWorld();
+
+
+var hamburgerBtn = document.querySelector('.hamburger');
+var headerNav = document.querySelector('.header-nav');
+
+// hamburgerBtn.classList.remove('is-active')
+
+
+hamburgerBtn.addEventListener('click', function() {
+
+    hamburgerBtn.classList.toggle('is-active');
+    headerNav.classList.toggle('header-nav--is-visible');
+
+
+})
+
+
+var backToTop = document.querySelector('.back-to-top');
+var offset = document.querySelector('.services').offsetTop;
+var offsetNav = document.querySelector('.site-header').offsetTop;
+
+window.addEventListener('scroll', function() {
+  console.log('window.scrollY', window.scrollY);
+  console.log('offsetNav', offsetNav);
+  if (window.scrollY > offset) {
+    backToTop.classList.add('back-to-top--is-visible');
+  } else {
+    backToTop.classList.remove('back-to-top--is-visible');
+  }
+
+
+  if (window.scrollY > offsetNav) {
+    document.body.classList.add('navigation-is-fixed');
+  } else {
+    document.body.classList.remove('navigation-is-fixed');
+  }
+})
+
+
+backToTop.addEventListener('click', function(event) {
+
+  event.preventDefault();
+
+  window.scroll({
+    top: 0,
+    behavior: 'smooth',
+  })
+})
+
+
+
+var links = document.querySelectorAll('.header-nav a');
+var servicesSection = document.querySelector('.services');
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function(event){
+    event.preventDefault();
+    console.log(event.target.hash);
+    var sectionID = event.target.hash;
+
+    var secrion = document.querySelector(sectionID); // #name section
+
+
+    secrion.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  })
+}
+
+// link.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   servicesSection.scrollIntoView({
+//     behavior: 'smooth',
+//     block: 'start'
+//   })
+// })
